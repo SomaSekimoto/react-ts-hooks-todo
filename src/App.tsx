4b2cd9daa009b89c13e2e71e6cfc7821f0be5134
SomaSekimoto <0421.soma@gmail.com>
2022-08-08 11:35:12 +0900

    add: state 'text'

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+// React から useState フックをインポート
+import { useState } from 'react';
+
 export const App = () => {
+  /**
+   * text = ステートの値
+   * setText = ステートの値を更新するメソッド
+   * useState の引数 = ステートの初期値 (=空の文字列)
+   */
+  const [text, setText] = useState('');
+
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+        {/*
+          入力中テキストの値を text ステートが
+          持っているのでそれを value として表示
+
+          onChange イベント（＝入力テキストの変化）を
+          text ステートに反映する
+         */}
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <input
           type="submit"
           value="追加"
