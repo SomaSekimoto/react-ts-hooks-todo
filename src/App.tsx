fcd9ef075399b9c94f6e4a128d09e300c8420792
SomaSekimoto <0421.soma@gmail.com>
2022-08-12 16:49:08 +0900

    create: list & key

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 type Todo = {
   value: string;
+  readonly id : number;
 };
 
 export const App = () => {
@@ -22,6 +23,7 @@ export const App = () => {
     // 新しい Todo を作成
     const newTodo: Todo = {
       value: text,
+      id: new Date().getTime(),
     };
 
     /**
@@ -42,6 +44,25 @@ export const App = () => {
     setText(e.target.value);
   };
 
+  const handleOnEdit = (id: number, value: string) => {
+    /**
+     * 引数として渡された todo の id が一致する
+     * todos ステート（のコピー）内の todo の
+     * value プロパティを引数 value (= e.target.value) に書き換える
+     */
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.value = value;
+      }
+      return todo;
+    });
+
+    console.log('=== Original todos ===');
+    todos.map((todo) => console.log(`id: ${todo.id}, value: ${todo.value}`));
+    // todos ステートを更新
+    setTodos(newTodos);
+  };
+
   return (
     <div>
      {/* コールバックとして () => handleOnSubmit() を渡す */}
@@ -55,6 +76,17 @@ export const App = () => {
         {/* 上に同じ */}
         <input type="submit" value="追加" onSubmit={handleOnSubmit} />
      </form>
+     <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}>
+                  <input
+                    type="text"
+                    value={todo.value}
+                    onChange={(e) => handleOnEdit(todo.id, e.target.value)}
+                  />
+                </li>
+        })}
+      </ul>
    </div>
   );
 };
